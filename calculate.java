f90e020095567f4a9527fc4143f43d6df8d71922
adi7392 <adiiigupta7392@gmail.com>
2026-08-10 22:02:06 +0530

    Initial commit

---
+++ calculate.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class calculate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        int div = a / b;
+
+        System.out.println("Addition = " + sum);
+        System.out.println("Subraction = " + sub);
+        System.out.println("Multiplication = " + mul);
+        System.out.println("Division = " + div);
+
+
+        sc.close();
+    }
+    
+}
