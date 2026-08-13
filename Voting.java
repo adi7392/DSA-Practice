614ab3295ba78b77eade66d8b39fdeeb25050b65
Aditya Gupta <adiiigupta7392@gmail.com>
2026-08-13 23:54:36 +0530

    Conditions

---
+++ Voting.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Voting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Age = ");
+        int age = sc.nextInt();
+
+        if( age >=18 ){
+            System.out.println("You are Eligible to Vote");
+        }else{
+            System.out.println("You are not Eligible to Vote");
+        }
+
+        sc.close();
+    }
+}
