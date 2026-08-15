6e9be642657d00d7d89ac4c4cc3f176fa7e39c34
Aditya Gupta <adiiigupta7392@gmail.com>
2026-08-15 22:47:24 +0530

    Loops

---
+++ OddSum.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class OddSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter The Number = ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for(int i = 1; i<=n; i++){
+            if(i % 2 != 0){
+                sum = sum + i;
+            }
+        }
+        System.out.println("Sum of all Odd Numbers are = " + sum);
+        sc.close();
+    }
+}
