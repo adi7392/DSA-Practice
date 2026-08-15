f8ae43b95184e1417b732ec004f4e069f6c7678a
Aditya Gupta <adiiigupta7392@gmail.com>
2026-08-15 16:37:44 +0530

    Loops

--- NaturalNumber.java
+++ NaturalNumber.java
@@ -5,7 +5,7 @@ public class NaturalNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the Number");
+        System.out.print("Enter the Number = ");
         int num = sc.nextInt();
 
         for( int i = 1; i <= num; i++ ){
