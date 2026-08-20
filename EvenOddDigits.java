f6943473274cec85d1e2b4f3663744f234e05ca1
Aditya Gupta <adiiigupta7392@gmail.com>
2026-08-20 22:43:25 +0530

    Level 4

---
+++ EvenOddDigits.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class EvenOddDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Eneter the Number = ");
+        int n = sc.nextInt();
+
+        int evencount = 0;
+        int oddcount = 0;
+
+        while ( n != 0 ){
+            int digit = n % 10;
+            if ( digit % 2 == 0){
+                evencount++;
+            }else{
+                oddcount++;
+            }
+            n = n / 10;
+        }
+        System.out.println("Even count = " + evencount);
+        System.out.println("Odd count = " + oddcount);
+        sc.close();
+    }
+}
