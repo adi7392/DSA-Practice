479e50c15739902439aed2fbfb8fb73ab556149c
Aditya Gupta <adiiigupta7392@gmail.com>
2026-09-06 07:39:01 +0530

    Level 5

---
+++ FirstNPrimeNumber.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class FirstNPrimeNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        int count = 0;
+        int number = 2;
+
+        while (count < n) {
+
+            int divisorCount = 0;
+
+            for (int i = 1; i <= number; i++) {
+                if (number % i == 0) {
+                    divisorCount++;
+                }
+            }
+
+            if (divisorCount == 2) {
+                System.out.print(number + " ");
+                count++;
+            }
+
+            number++;
+        }
+
+        sc.close();
+    }
+}
