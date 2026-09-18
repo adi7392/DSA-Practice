a33560be0a3bb0d43d04e8c10e95c61b53fddc4c
Aditya Gupta <adiiigupta7392@gmail.com>
2026-09-18 06:56:07 +0530

    Level 7

---
+++ ReplaceCharacter.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class ReplaceCharacter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String = ");
+        String str = sc.nextLine();
+
+        System.out.print("Enter Character to Replace = ");
+        char oldChar = sc.next().charAt(0);
+
+        System.out.print("Enter new Character = ");
+        char newChar = sc.next().charAt(0);
+
+        String result = "";
+
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+
+            if(ch == oldChar){
+                result = result + newChar;
+            }else{
+                result = result + ch;
+            }
+        }
+        System.out.println("Updated String = " + result);
+        sc.close();
+    }
+}
